37157b0b9740510454b2e766dc500abccc93bde4
erk.mustech <erk.mustech@gmail.com>
2023-03-22 01:57:21 +0100

    terraform aws doker instance

---
+++ main.tf
@@ -0,0 +1,38 @@
+
+resource "aws_instance" "tfmyec2" {
+  ami = data.aws_ami.amazon-linux-2.id
+  instance_type = var.instance_type
+  count = var.num_of_instance
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  user_data = data.template_file.userdata.rendered
+  tags = {
+    Name = var.tag
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "${var.tag}-terraform-sec-grp"
+  tags = {
+    Name = var.tag
+  }
+
+  dynamic "ingress" {
+    for_each = var.docker-instance-ports
+    iterator = port
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port =0
+    protocol = "-1"
+    to_port =0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
